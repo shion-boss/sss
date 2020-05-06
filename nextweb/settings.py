aa1a3cbc791a785ed50b772055969f132d9bd47e
shion-boss <sss.tl.ges.sss@gmail.com>
2020-05-07 04:28:07 +0900

    Update settings.py

--- nextweb/settings.py
+++ nextweb/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import allauth
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'techbee', 
+    'techbee',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'allauth.socialaccount.providers.line',
     'rest_framework',
 ]
