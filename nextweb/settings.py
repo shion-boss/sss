ba2cbdbc527f4c838986ed19e1a9861abac82a1b
shion-boss <sss.tl.ges.sss@gmail.com>
2020-05-07 21:48:30 +0900

    Update settings.py

--- nextweb/settings.py
+++ nextweb/settings.py
@@ -100,16 +100,7 @@ DATABASES = {
     }
 }
 
-DEBUG = True
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
-
-if not DEBUG:
-    import django_heroku
-    django_heroku.settings(locals())
+DEBUG = False
 
 
 # Password validation
