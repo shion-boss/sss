2858ffa98e6dea5416b448d136d50a3163814135
shion-boss <sss.tl.ges.sss@gmail.com>
2020-05-09 07:11:51 +0900

    Update settings.py

--- nextweb/settings.py
+++ nextweb/settings.py
@@ -177,7 +177,6 @@ MEDIA_URL = '/media/'
 
 
 import dj_database_url
-DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
@@ -190,3 +189,6 @@ try:
     from .local_settings import *
 except ImportError:
     pass
+
+db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'].update(db_from_env)
