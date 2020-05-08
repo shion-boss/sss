dbbcf63e8c503f11bd66f7afa1d45acfe8b28d87
shion-boss <sss.tl.ges.sss@gmail.com>
2020-05-09 07:54:28 +0900

    Update settings.py

--- nextweb/settings.py
+++ nextweb/settings.py
@@ -84,12 +84,12 @@ WSGI_APPLICATION = 'nextweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
