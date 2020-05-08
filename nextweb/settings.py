325ff2a621981c45392a5d9254e85679d6a87f05
shion-boss <sss.tl.ges.sss@gmail.com>
2020-05-09 04:21:53 +0900

    Update settings.py

--- nextweb/settings.py
+++ nextweb/settings.py
@@ -193,6 +193,6 @@ except ImportError:
     pass
 
 if not DEBUG:
-    SECRET_KEY = os.environ['bklw7@pfms0u=+d2%jpg(gp%--+=!l0#n%m70+7=d#9gx_#=_z']
+    SECRET_KEY = os.environ['SECRET_KEY']
     import django_heroku #追加
     django_heroku.settings(locals()) #追加
