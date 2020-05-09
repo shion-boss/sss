d0431acc48943d8494c93347f646695e897055b1
shion-boss <sss.tl.ges.sss@gmail.com>
2020-05-10 04:24:57 +0900

    Update urls.py

--- nextweb/urls.py
+++ nextweb/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from techbee.urls import router
+from django.views.static import serve  #追加
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,12 +24,10 @@ urlpatterns = [
     path('auth/', include('allauth.urls')),
     path('oauth/', include('social_django.urls', namespace='social')),
     path('api/',include(router.urls)),
+    path('media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 from django.conf import settings  # 追加
 from django.conf.urls.static import static # 追加
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-from django.views.static import serve  #追加
-urlpatterns +=path('media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
