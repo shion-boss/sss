0c12b376130cb0541ea5b9cf92e93510f072270e
shion-boss <sss.tl.ges.sss@gmail.com>
2020-05-12 19:45:01 +0900

    Update settings.py

--- nextweb/settings.py
+++ nextweb/settings.py
@@ -11,9 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
