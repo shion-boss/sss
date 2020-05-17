c7f65e1d6447abdabf2e09bb2ba7a028636bfa08
shion-boss <sss.tl.ges.sss@gmail.com>
2020-05-18 00:01:11 +0900

    Update models.py

--- techbee/models.py
+++ techbee/models.py
@@ -15,8 +15,8 @@ class user_meta(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
     username=models.CharField(max_length=20)
     photo=CloudinaryField('image',null=True, blank=True,default="https://res.cloudinary.com/hm9k6tunn/image/upload/v1589727150/defo_ebe9rs.jpg")
-    name=models.CharField(max_length=20,null=True, blank=True)
-    plofile=models.TextField(max_length=220,null=True, blank=True)
+    name=models.CharField(max_length=20,null=True, blank=True,default='')
+    plofile=models.TextField(max_length=220,null=True, blank=True,default='')
     point=models.IntegerField(default=0)
     like_point=models.IntegerField(default=100)
     give_like=models.IntegerField(default=1,validators=[MinValueValidator(1), MaxValueValidator(10)])
@@ -27,7 +27,7 @@ class user_meta(models.Model):
 class categories_model(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     categories=models.CharField(max_length=30)
-    img=CloudinaryField('image',null=True, blank=True)
+    img=CloudinaryField('image',null=True, blank=True,default="https://res.cloudinary.com/hm9k6tunn/image/upload/v1589727150/defo_ebe9rs.jpg")
     post_time=models.DateTimeField(default=timezone.datetime.now)
 
 
@@ -35,7 +35,7 @@ class parts_model(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     categories=models.ForeignKey(categories_model,on_delete=models.SET_NULL,blank=True,null=True)
     file_name=models.CharField(max_length=30)
-    image =CloudinaryField('image', null=True, blank=True)
+    image =CloudinaryField('image', null=True, blank=True,default="https://res.cloudinary.com/hm9k6tunn/image/upload/v1589727150/defo_ebe9rs.jpg")
     codepen=models.TextField(max_length=500,default='',null=True, blank=True)
     text=models.TextField(max_length=3000,default='',null=True, blank=True)
     like_count=models.IntegerField(default=0)
@@ -61,7 +61,7 @@ class afirieito_model(models.Model):
     introducer=models.CharField(max_length=20)
 
 class event_model(models.Model):
-    img=CloudinaryField('image',null=True, blank=True)
+    img=CloudinaryField('image',null=True, blank=True,default="https://res.cloudinary.com/hm9k6tunn/image/upload/v1589727150/defo_ebe9rs.jpg")
     event_name=models.CharField(max_length=15)
     event_date=models.CharField(max_length=15)
     event_time=models.CharField(max_length=15)
