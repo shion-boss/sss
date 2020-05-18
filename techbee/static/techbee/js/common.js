cc57569b58b66300f508479c0b2359e23349a818
shion-boss <sss.tl.ges.sss@gmail.com>
2020-05-18 16:00:39 +0900

    tomoya

---
+++ techbee/static/techbee/js/common.js
@@ -0,0 +1,68 @@
+$('.slick01').slick( {
+  arrows: false,
+  dots:false,
+  autoplay: true,
+  pauseOnFocus:false,
+  autoplaySpeed:0,
+  adaptiveHeight: true,
+  cssEase:'linear',
+  speed:8000,
+  slidesToShow:4,
+  slidesToScroll:1,
+  infinite:true,
+  accessibility: false,
+  rtl:true,
+  responsive: [
+  {
+   breakpoint: 768, //767px以下のサイズに適用
+   settings: {
+     slidesToShow:3
+   }
+  }
+]
+});
+$('.slick02').slick( {
+  arrows: false,
+  dots: false,
+  autoplay: true,
+  pauseOnFocus:false,
+  autoplaySpeed:0,
+  adaptiveHeight: true,
+  cssEase:'linear',
+  speed:12000,
+  slidesToShow:4,
+  slidesToScroll:1,
+  accessibility: false,
+  infinite:true,
+  responsive: [
+  {
+   breakpoint: 768, //767px以下のサイズに適用
+   settings: {
+     slidesToShow:3
+   }
+  }
+]
+});
+$('.slick03').slick( {
+  arrows: false,
+  dots:false,
+  autoplay: true,
+  pauseOnFocus:false,
+  autoplaySpeed:0,
+  adaptiveHeight: true,
+  cssEase:'linear',
+  speed:12000,
+  slidesToShow:4,
+  slidesToScroll:1,
+  infinite:true,
+  accessibility: false,
+  rtl:true,
+  responsive: [
+  {
+   breakpoint: 768, //767px以下のサイズに適用
+   settings: {
+     slidesToShow:3
+   }
+  }
+]
+});
