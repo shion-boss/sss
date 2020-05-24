e71d37353ef274e3cc25ff322a5a9f5b5cd3258e
shion-boss <sss.tl.ges.sss@gmail.com>
2020-05-25 06:15:15 +0900

    Update settings.py

--- nextweb/settings.py
+++ nextweb/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'techbee',
-    'bee_tag',
+    'templatetags',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
